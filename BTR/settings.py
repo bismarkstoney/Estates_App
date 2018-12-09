c5c7d777391166e117d76581e7d16eefd07bed44
Bismark Obeng <bismarkstoney@yahoo.com>
2018-12-09 04:46:07 +0000

    change setting files

--- BTR/settings.py
+++ BTR/settings.py
@@ -147,6 +147,6 @@ MESSAGE_TAGS = {
 # Email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "obengstoney@gmail.com"
-EMAIL_HOST_PASSWORD = 'Winsrv2008'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
